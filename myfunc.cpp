8221e16539c558c6ae3c8ca2c24518fbfffb11d8
Joshua <joshualuzier@gmail.com>
2021-09-10 18:01:40 -0400

    added new files

---
+++ myfunc.cpp
@@ -0,0 +1,14 @@
+/*
+ * myfunc.cpp
+ *
+ *  Created on: Sep 9, 2021
+ *      Author: josh
+ */
+#include <iostream>
+
+std::string func(){
+	//say hello
+	return "hello";
+}
+
+
---
+++ myfunc.h
@@ -0,0 +1,15 @@
+/*
+ * myfunc.h
+ *
+ *  Created on: Sep 9, 2021
+ *      Author: josh
+ */
+
+#ifndef MYFUNC_H_
+#define MYFUNC_H_
+
+std::string func();
+
+
+
+#endif /* MYFUNC_H_ */
